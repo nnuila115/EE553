8fa0254e09dbdd2bab4af71e1ad6624b7bb8dec9
nnuila115 <nnuila@stevens.edu>
2017-02-12 01:45:02 -0500

    updated, still needs to fix angle calculation

--- HWEE553/03chw.cpp
+++ HWEE553/03chw.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-class MrRoboto{
-private:
-  double px, py;  //position x and position y
+  double px = 0;
+  double py = 0;  //position x and position y
 
-public:
-  friend double changePosition(){
+  void changePosition(){
     double angle = 0;
     double distance = 0;
-    cout << "Please enter the direction you would like MrRoboto to move (in degrees, north = 0, East = 90) and a the distance in that direction." << endl;
-    cin >> angle >> distance;
-    
+    while (angle != -1 && distance != -1){
+      cout << "Please enter the direction you would like MrRoboto to move (in degrees, north = 0, East = 90) and the distance in that direction." << endl;
+      cout << "Enter -1 and -1 to exit." << endl;
+      cin >> angle >> distance;
+      while(cin.fail() || -360 > angle > 360 ){
+      cin.clear();
+          cout << "Error, please enter an angle between 0 and 360 and a positive distance." << endl;
+          cin.ignore(256,'\n');
+          cin >> angle >> distance;
+      }
+      angle = pow(angle,2);
+      angle = sqrt(angle);
+      distance = pow(distance,2);
+      distance = sqrt(distance);
+      angle = angle - 90;
+      if (90 < angle < 270)
+        angle *= -1;
+      angle *= M_PI / 180;
+      px = px + cos(angle) * distance;
+      py = py + sin(angle) * distance;
+      cout << "MrRoboto is at x=" << px << " and y=" << py << endl;
+      cout << '\n';
+    }
   }
-  double getPosition(){
 
-  }
-};
 
 int main(){
-
+  changePosition();
 }
