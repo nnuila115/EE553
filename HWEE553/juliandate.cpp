f3f19f616281ab15f19270e1f1031c41636b1d07
nnuila115 <nnuila@stevens.edu>
2017-04-02 14:56:42 -0400

    updating hw files

--- HWEE553/juliandate.cpp
+++ HWEE553/juliandate.cpp
@@ -22,7 +22,7 @@ private:
 
 public:
   JulianDate(int year, int month, int day, int hour, int minute, int second) : year(year), month(month), day(day), hour(hour), minute(minute), second(second) {}
-  
+
 };
 
 int main() {
